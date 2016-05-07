5fe72a0bbb40ce3b14aeb4abf62cee67e457258e
bibi21000 <bibi21000@gmail.com>
2016-05-07 21:25:32 +0200

    Update db tests for docker

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -36,8 +36,15 @@ import logging
 
 from janitoo_nosetests import JNTTBase
 from janitoo_nosetests.server import JNTTDockerServerCommon, JNTTDockerServer
+from janitoo_nosetests.models import jntt_docker_models
 
 from janitoo.runner import Runner, jnt_parse_args
 from janitoo.server import JNTServer
 from janitoo.utils import HADD_SEP, HADD
 
+sys.path.insert(0, os.path.abspath('.'))
+
+from test_models import ModelsCommon
+
+#Launch ModelsCommon tests for every supported database
+jntt_docker_models(__name__, ModelsCommon)
